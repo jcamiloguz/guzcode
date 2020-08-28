3ad4464afc8039c99bc7e89c17331d68030492e6
Juan Camilo Guzman <jcamiloguz1@gmail.com>
2020-08-28 13:25:13 -0500

    Home text and skeleton footer

---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../assets/styles/components/Hero.scss'
+export const Hero = () => {
+  return (
+    <section className="hero">
+      <h1>Juan Camilo Guzman</h1>
+      <h2>Software Developer</h2>
+    </section>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,20 @@
 import React from 'react'
+import { Hero } from '../components/Hero'
+import '../assets/styles/components/Home.scss'
 
-export const Home = () => <h1>Hi</h1>
+export const Home = () => {
+  return (
+    <>
+      <Hero></Hero>
+      <main className="main">
+        <p>
+          I am a <span>software developer</span> currently focused on web
+          technologies with a high degree of adaptability and responsibility,
+          with the ability to work as a team and autonomously, passionate about
+          new technologies. I am looking to collaborate as a programmer with
+          challenging problems and developing <span>quality software</span>.
+        </p>
+      </main>
+    </>
+  )
+}
